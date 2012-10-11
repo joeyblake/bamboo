2d30af1f23a06053526f836241c5401e9807be99
Joey Blake <joeyblake@gmail.com>
2012-10-10 22:29:09 -0400

    plugin boilerplate, stripe library, stripe button css

---
+++ includes/api.php
@@ -0,0 +1,98 @@
+<?
+# RESTful API - not to be confused with AJAX stuff
+class BbApi_v1 extends AbstractBbApi {
+
+}
+
+abstract class AbstractBbApi {
+  static function _method() {
+    $headers = apache_request_headers();
+    if (!empty($headers['X-HTTP-Method-Override'])) {
+      return strtolower($headers['X-HTTP-Method-Override']);
+    } else if (!empty($_REQUEST['_method'])) {
+      return strtolower(trim($_REQUEST['_method']));
+    } else {
+      return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+  }
+
+  function _assertLoggedIn() {
+    if (!is_user_logged_in()) {
+      wp_redirect(wp_login_url($_SERVER['REQUEST_URI']));
+      exit;
+    }
+  }
+
+  function _isAdmin() {
+    return buf_current_user_is_admin();
+  }
+
+  function _assertIsAdmin() {
+    if (!$this->_isAdmin()) {
+      wp_redirect(wp_login_url($_SERVER['REQUEST_URI']));
+      exit;
+    }
+  }
+
+  function _isGet() {
+    return self::_method() === 'get';
+  }
+
+  function _isPost() {
+    return self::_method() === 'post';
+  }
+
+  function _isPut() {
+    return self::_method() === 'put';
+  }
+
+  function _isDelete() {
+    return self::_method() === 'delete';
+  }
+}
+
+add_filter('rewrite_rules_array', 'bb_rewrite_rules_array');
+add_filter('query_vars', 'bb_query_vars');
+add_action(constant('WP_DEBUG') || constant('BB_DEBUG') ? 'wp_loaded' : 'bb_activated', 'bb_flush_rewrite_rules');
+add_action('parse_request', 'bb_parse_request');
+
+// e.g., bb/1/profiles/:id
+define('BB_API_REWRITE_RULE', '(sp)/(\d)/([a-z]+)(/.*?)?(.json)?$');
+
+function bb_rewrite_rules_array($rules) {
+  return array(
+    BB_API_REWRITE_RULE => 'index.php?_sp=$matches[3]&_v=$matches[2]&_args=$matches[4]'
+  ) + $rules;
+}
+
+function bb_query_vars($vars) {
+  $vars[] = '_bb';
+  $vars[] = '_v';
+  $vars[] = '_args';
+  return $vars;
+}
+
+function bb_flush_rewrite_rules() {
+  $rules = get_option( 'rewrite_rules' );
+  if (!isset($rules[BB_API_REWRITE_RULE])) {
+    global $wp_rewrite;
+    $wp_rewrite->flush_rules();
+  }
+}
+
+function bb_parse_request($wp) {
+  if (isset($wp->query_vars['_sp'])) {    
+    $class = "BbApi_v{$wp->query_vars['_v']}";
+    if (!class_exists($class)) {
+      return;
+    }
+    $api = new $class();
+    $fx = array($api, $wp->query_vars['_sp']);
+    if (is_callable($fx)) {
+      $result = call_user_func_array($fx, array_filter(explode('/', $wp->query_vars['_args'])));
+      header('Content-Type: application/json');
+      echo json_encode($result);
+      exit(0);
+    }  
+  }
+}
---
+++ includes/bamboo.php
@@ -0,0 +1,38 @@
+<?php
+add_action('init', 'bam_init');
+add_action('admin_bar_menu', 'bam_admin_bar_menu', 1000);
+add_action('wp_enqueue_scripts', 'bam_wp_enqueue_scripts');
+add_action('admin_enqueue_scripts', 'bam_wp_enqueue_scripts');
+
+function bam_init() {
+  
+  register_post_type('bb_purchases', array(
+    'public' => false,
+    'publicly_queryable' => false,
+    'show_ui' => false, 
+    'show_in_menu' => false, 
+    'query_var' => false,
+    'rewrite' => false,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false,
+    'menu_position' => null
+  ));
+
+  register_post_type('bb_products', array(
+    'public' => false,
+    'publicly_queryable' => false,
+    'show_ui' => false, 
+    'show_in_menu' => false, 
+    'query_var' => false,
+    'rewrite' => false,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false,
+    'menu_position' => null
+  ));
+}
+
+function bam_wp_enqueue_scripts() {
+
+}
